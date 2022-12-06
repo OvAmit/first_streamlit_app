445da9cd1d2de91126634c60830a2c947e09a921
Amit Ovhal <119920080+OvAmit@users.noreply.github.com>
2022-12-06 15:29:59 +0530

    Update streamlit_app.py
    added one more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit  
  
 #streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Dinner')
 streamlit.header('Breakfast favorites') 
 #streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
